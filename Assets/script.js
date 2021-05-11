0f9f7c433e0fc63352e8ae9bb6d365819a8f62cf
daze_77 <daze77@gmail.com>
2021-05-11 11:01:20 -0400

    added gitignore, updated code so that it is easier to read and refactored some code for the same purpose

--- Assets/script.js
+++ Assets/script.js
@@ -3,42 +3,47 @@
 var startBtn = document.querySelector("#goTime");
 console.log("Page Loaded")
 
-
-
 //global variables to be used for entire JS
 
-var questions = [
-    "1. JavaScript is a ___ -side programming language",
-    "2. How to write an if statement in javaScript?",
-    "3. What will 'Boolean(3 < 7)' return?",
-    "4. How do you write a comment in javaScript?",
-    "5. What is the correct why to write if 'i' is not equal to 8",
-    "6. How do you call a function named 'myFunction?'"
-];
+var questionsAndAnswers = [
+  {
+    Question: "1. JavaScript is a ___ -side programming language",
+    Answers: ["Client","Server","Both","None"],
+    CorrectAnswer: "Both",
+  },
+  {
+    Question:  "2. How to write an if statement in javaScript?",
+    Answers:  ["if i = 5","if i == 5 then","if i = 5 then","if (i==5)"],
+    CorrectAnswer: "if (i==5)",
+  },
+  {
+    Question: "3. What will 'Boolean(3 < 7)' return?",
+    Answers:  ["true","false","NaN","SyntaxError"],
+    CorrectAnswer:  "true",
 
-var questionresponses = [
-    ["Client","Server","Both","None"],
-    ["if i = 5","if i == 5 then","if i = 5 then","if (i==5)"],
-    ["true","false","NaN","SyntaxError"],
-    ['\\Like this-->', "Like this", "//Like this"],
-    ["if (i !=8)", "if i <> 8", "if i = ! 8 then", "if (i<>8)"],
-    ["call myFunction()","myFunction()","call function myFunction()"]
-]
-
-var questionanswers = [
-    "Both",
-    "if (i==5)",
-    "true",
-    "//Like this",
-    "if (i !=8)",
-    "myFunction()"
+  },
+  {
+    Question: "4. How do you write a comment in javaScript?",
+    Answers: ['\\Like this-->', "Like this", "//Like this"],
+    CorrectAnswer:  "//Like this",
+ 
+  },
+  {
+    Question: "5. What is the correct why to write if 'i' is not equal to 8",
+    Answers: ["if (i !=8)", "if i <> 8", "if i = ! 8 then", "if (i<>8)"],
+    CorrectAnswer:  "if (i !=8)",
+   
+  },
+  {
+    Question: "6. How do you call a function named 'myFunction?",
+    Answers: ["call myFunction()","myFunction()","call function myFunction()"],
+    CorrectAnswer:  "myFunction()"
+  }
 ]
 
 var score
 var timer
 var personsname
-
-
 
 function startGame(){
     console.log ("Start button pressed")
@@ -51,6 +56,8 @@ function startGame(){
     document.querySelector('#timer').classList.remove('d-none')
     document.querySelector('#timerheader').classList.remove('d-none')
     document.querySelector('#goTime').classList.add('d-none')
+    document.querySelector('#startMessage').classList.add('d-none')
+
     document.querySelector('#score').classList.remove('d-none')
     document.querySelector('#scoreheader').classList.remove('d-none')
     document.querySelector('#timer').textContent = timer
@@ -59,31 +66,31 @@ function startGame(){
 
     timeercountdown = setInterval(timerFunction, 1000)
 
-    questionroulette()
+    // questionroulette()
+    nextQuestion()
 
 }
 
-function questionroulette(){
-    var currentquestion = questions[questionNo]
-    var questionEl = document.querySelector('.nextQuestion')
-    // display the question within the <h3> header
-    questionEl.innerHTML = `<h3>${currentquestion}</h3><br><br>`
-  
-    // loop through and show each answer as a button
-    for( var i=0; i < questionresponses[questionNo].length; i++ ){
-      var availableresponses = questionresponses[questionNo][i]
-      
-      questionEl.innerHTML += 
-      `<button onClick="selectAnswer(event,'${availableresponses}')" class="btn btn-primary mx-1" type="button">${availableresponses}</button>`
-    }
+function nextQuestion(){
+  var questionEL = document.querySelector('.nextQuestion')
+  var nextQuestion = questionsAndAnswers[questionNo]
+    questionEL.innerHTML = `
+    <h3>${nextQuestion.Question}</h3><br><br>
+    `
+
+  for( var i=0; i<nextQuestion.Answers.length; i++){
+    questionEL.innerHTML += 
+    `<button onClick="selectAnswer(event, '${nextQuestion.Answers[i]}')" class="btn btn-primary mx-1" type="button">${nextQuestion.Answers[i]}</button>`
+  }
 }
 
 
-function selectAnswer( event,availableresponses ){
+
+function selectAnswer( event,selectedAnswer ){
     event.preventDefault()
-    console.log( `question answer id: ${availableresponses}` )
-    if( availableresponses===questionanswers[questionNo] ){
-      console.log( `Nice! Correct answer: ${availableresponses}` )
+    console.log( `question answer id: ${selectedAnswer}` )
+    if( selectedAnswer===questionsAndAnswers[questionNo].CorrectAnswer ){
+      console.log( `Nice! Correct answer: ${selectedAnswer}` )
       scoreCounter(1)
     } else {
       console.log( `Nope, penatly, -10 seconds` )
@@ -91,8 +98,8 @@ function selectAnswer( event,availableresponses ){
     }
     questionNo++
     // show next question or quit game if there are no further questions left
-    if( questionNo<questions.length )
-      questionroulette()
+    if( questionNo<questionsAndAnswers.length )
+      nextQuestion()
     else
       done()
   }
@@ -119,6 +126,7 @@ function done(){
     console.log("Congratulations you finished! Now breathe!")
     document.querySelector('#quizOver').classList.remove('d-none')
     document.querySelector('#questioncards').classList.add('d-none')
+
     clearInterval(timeercountdown)
     
     //save the scores to local storage
@@ -128,23 +136,22 @@ function done(){
       console.log(scoreboard)
       //add current inform to scoreboard - via push
       var yourscore = {
-        scoreboardname: personsname,
-        scoreboardscore: score,
+        name: personsname,
+        score: score,
       };
       console.log(yourscore)
       //this is the push
       scoreboard.push(yourscore);
       //put new scoreboard back into local storage
-      localStorage.setItem("scoreboard", JSON.stringify(scoreboard));
+      localStorage.scoreboard=JSON.stringify(scoreboard);
 
       scoreboard.sort(function(a,b){
-        return b.scoreboardscore - a.scoreboardscore;
+        return b.score - a.score;
       });
-      scoreboard.forEach(function(scoreboardscore){
-        var litag = document.createElement("li");
-        litag.textContent= scoreboardscore.scoreboardname + "-" + scoreboardscore.scoreboardscore;
-        var lsb = document.querySelector("#scoreboard");
-        lsb.appendChild(litag);
+      scoreboard.forEach(function(item){
+        document.querySelector('#scoreboard').innerHTML +=`
+        <li class="list-group-item px-5">${item.name} <span class="float-end">${item.score}</span></li>
+        `
       })
 
 
@@ -158,29 +165,6 @@ function restart(event){
   
     location.reload();
 }
-
-//new code I forgot to build - log user name and score
-
-
-
-// var scoreboard = {
-//   scoreboardname: document.querySelector("#name").value,
-//   scoreboardscore: document.querySelector("#score").value,
-//   }
-
- 
-// console.log( `scoreboard contents: `, scoreboard )
-// localStorage.scoreboard = JSON.stringify( scoreboard )
-// console.log( `.. scoreboard saved as: `, localStorage.scoreboard )
-
-
-
-
-// if( localStorage.scoreboard ){
-//   var scoreboard = JSON.parse( localStorage.scoreboard )
-//   console.log( ` found saved Scoreboard: `, scoreboard )
-// }
-
 
 
 startBtn.addEventListener("click", function(event){
